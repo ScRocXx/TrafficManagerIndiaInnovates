bc4ad082d36fb52f2d1f572d75020dd3d57e430b
Tushar <tusharkr7362@gmail.com>
2026-03-27 11:38:28 +0530

    style(map): switch tile provider to Stadia Maps Alidade Smooth

--- frontend/src/components/MapComponent.tsx
+++ frontend/src/components/MapComponent.tsx
@@ -270,10 +270,10 @@ export default function MapComponent({ onSelectIntersection, selectedIntersectio
         <TileLayer
           key={isDark ? "dark" : "light"}
           url={isDark
-            ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-            : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            ? "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+            : "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
           }
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
 
         <TelemetryMarkers onSelectIntersection={onSelectIntersection} isDark={isDark} />
